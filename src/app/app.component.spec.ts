9eb891f6b65c5819b57c3a9ee9804e3523a3c544
Jack.Deazley.76009261 <jack.deazley@mirofocus.com>
2022-12-06 17:20:24 +0000

    fixed upp app component, added in header & search

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,126 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { ContactsListModule } from './components/contacts-list/contacts-list.module';
+import { SearchContactsModule } from './components/search-contacts/search-contacts.module';
+import { ContactService } from './services/contacts.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      declarations: [AppComponent],
+      imports: [
+        HttpClientTestingModule,
+        FontAwesomeModule,
+        SearchContactsModule,
+        ContactsListModule,
+        CollapseModule,
+        BrowserAnimationsModule,
       ],
+      providers: [ContactService],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'foundry-address-book'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('foundry-address-book');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foundry-address-book app is running!');
+  });
+  describe('getHeroes', () => {
+    it('AC001 - should get the default array from mock db', () => {
+      // arrange
+      const contacts = [
+        {
+          id: '1',
+          firstName: 'Tony',
+          lastName: 'Stark',
+        },
+        {
+          id: '2',
+          firstName: 'Steve',
+          lastName: 'Rodgers',
+        },
+        {
+          id: '3',
+          firstName: 'Carol',
+          lastName: 'Danvers',
+        },
+        {
+          id: '4',
+          firstName: 'Bruce',
+          lastName: 'Banner',
+        },
+        {
+          id: '5',
+          firstName: 'Thor',
+          lastName: 'Odinson',
+        },
+      ];
+
+      const spyOnGetContacts = spyOn(
+        component.contactService,
+        'getContacts'
+      ).and.returnValue(of(contacts));
+
+      // act
+      component.getHeroes();
+
+      // assert
+      expect(spyOnGetContacts).toHaveBeenCalled();
+      expect(component.contacts).toEqual(contacts);
+    });
+
+    it('AC002 - should filter the existing array', () => {
+      // arrange
+
+      const contactList = [
+        {
+          id: '1',
+          firstName: 'Tony',
+          lastName: 'Stark',
+        },
+        {
+          id: '2',
+          firstName: 'Steve',
+          lastName: 'Rodgers',
+        },
+        {
+          id: '3',
+          firstName: 'Carol',
+          lastName: 'Danvers',
+        },
+      ];
+
+      component.contacts = contactList;
+
+      component.contactList = {
+        contacts: contactList,
+      } as ContactsListComponent;
+
+      // act
+      component.getHeroes('Ca');
+
+      // assert
+      expect(component.contactList.contacts).toEqual([contactList[2]]);
+    });
+  });
+
+  describe('ngOnInit', () => {
+    it('AC003 - should call the getHeroes method', () => {
+      // arrange
+      const spyOnGetHeroes = spyOn(component, 'getHeroes');
+
+      // act
+      component.ngOnInit();
+
+      // assert
+      expect(spyOnGetHeroes).toHaveBeenCalled();
+    });
   });
 });
